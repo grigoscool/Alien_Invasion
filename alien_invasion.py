ac8eb00c4ca5504a0a73b344a5fd3c4f730f0b0f
Sergey Grigorev <grigoscool@mail.ru>
2022-12-04 09:20:54 +0300

    First commit

---
+++ alien_invasion.py
@@ -0,0 +1,57 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """Класс для управления ресурсами игры."""
+
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы."""
+        pygame.init()
+        self.settings = Settings()
+
+        # Создает игровое окно
+        self.screen = pygame.display.set_mode((self.settings.screen_widht, self.settings.screen_heighr))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Запуск осного цикла игры."""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        # Отслеживание событий клавиатуры и мыши
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        # При каждом проходе цикла перериросвывать экран
+        self.screen.fill(self.settings.bg_color)
+        # при каждом проходу цикла перерисовывается корабль
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
+
+
+
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    '''Класс для сохранения всех настроек игры Alien Invasion.'''
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # параметры экрана
+        self.screen_widht = 1500
+        self.screen_heighr = 800
+        self.bg_color = (230,230,230)
